11b7755a8caf88ebf9350b43bcd2d7568930976f
aRailsDemo <arailsdemo@gmail.com>
2011-01-07 13:35:10 -0800

    Mock Models

---
+++ spec/views/posts/index.html.haml_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe "posts/index.html.haml" do
+  let(:post1) do
+    mock_model('Post',
+      :sequence => 1,
+      :title => 'First Post',
+      :description => 'Description1',
+      :created_at => Time.utc(2000))
+  end
+
+  let(:post2) do
+    mock_model('Post',
+      :sequence => 2,
+      :title => 'Second Post',
+      :description => 'Description2',
+      :created_at => Time.utc(2001))
+  end
+end
